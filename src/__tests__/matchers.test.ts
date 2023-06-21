9363497ec73d1fcda21e12e74372bcd3a3040757
penpenpng <poman1638m@gmail.com>
2023-06-22 00:12:20 +0900

    Add tests

--- src/__tests__/matchers.test.ts
+++ src/__tests__/matchers.test.ts
@@ -8,6 +8,7 @@ import '../matchers';
 import chalk from 'chalk';
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 
+import { deriveToHaveReceivedMessage, deriveToReceiveMessage } from '../matchers';
 import WS from '../websocket';
 
 let server: WS, client: WebSocket;
@@ -274,5 +275,109 @@ Expected the websocket object to be a valid WS mock.
 Received: string
   [31m\\"boom\\"[39m"
 `);
+  });
+});
+
+describe('A custom matcher `toReceiveHello` derived from toReceiveMessage', () => {
+  expect.extend({
+    toReceiveHello: deriveToReceiveMessage('toReceiveHello', function (received) {
+      const pass = received === 'Hello';
+      const message = pass
+        ? () => `Expected the next received message is not Hello, but got ${received}`
+        : () => `Expected the next received message is Hello, but got ${received}`;
+
+      return {
+        actual: received,
+        expected: 'Hello',
+        message,
+        pass,
+      };
+    }),
+  });
+
+  it('passes when received "Hello"', async () => {
+    client.send('Hello');
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await (expect(server) as any).toReceiveHello();
+  });
+
+  it('fails when received "Hi!"', async () => {
+    client.send('Hi!');
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await expect((expect(server) as any).toReceiveHello()).rejects.toThrowErrorMatchingInlineSnapshot(
+      '"Expected the next received message is Hello, but got Hi!"'
+    );
+  });
+
+  it('fails when received "Hello" under .not context', async () => {
+    client.send('Hello');
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await expect((expect(server) as any).not.toReceiveHello()).rejects.toThrowErrorMatchingInlineSnapshot(
+      '"Expected the next received message is not Hello, but got Hello"'
+    );
+  });
+
+  it('passes when received "Hi!" under .not context', async () => {
+    client.send('Hi!');
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await (expect(server) as any).not.toReceiveHello();
+  });
+});
+
+describe('A custom matcher `toHaveHello` derived from toHaveReceivedMessages', () => {
+  expect.extend({
+    toHaveHello: deriveToHaveReceivedMessage('toHaveHello', function (received) {
+      const pass = received.includes('Hello');
+      const message = pass
+        ? () => `Expected the WS server to not have received Hello, but got ${received}`
+        : () => `Expected the WS server to have received Hello, but got ${received}`;
+
+      return {
+        actual: received,
+        expected: ['Hello'],
+        message,
+        pass,
+      };
+    }),
+  });
+
+  it('passes when received "Hello"', async () => {
+    client.send('Hi!');
+    client.send('Hello');
+    await server.nextMessage;
+    await server.nextMessage;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (expect(server) as any).toHaveHello();
+  });
+
+  it('fails when not received "Hello"', async () => {
+    client.send('Hi!');
+    client.send('Yo');
+    await server.nextMessage;
+    await server.nextMessage;
+    expect(() => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (expect(server) as any).toHaveHello();
+    }).toThrowErrorMatchingInlineSnapshot('"Expected the WS server to have received Hello, but got Hi!,Yo"');
+  });
+
+  it('fails when received "Hello" under .not context', async () => {
+    client.send('Hi!');
+    client.send('Hello');
+    await server.nextMessage;
+    await server.nextMessage;
+    expect(() => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (expect(server) as any).not.toHaveHello();
+    }).toThrowErrorMatchingInlineSnapshot('"Expected the WS server to not have received Hello, but got Hi!,Hello"');
+  });
+
+  it('passes when not received "Hello" under .not context', async () => {
+    client.send('Hi!');
+    client.send('Yo');
+    await server.nextMessage;
+    await server.nextMessage;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (expect(server) as any).not.toHaveHello();
   });
 });
