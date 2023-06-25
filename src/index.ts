aa282d6b6be42d05278104d33e8ed1cab748e941
penpenpng <poman1638m@gmail.com>
2023-06-25 17:51:04 +0900

    Export matcher factories

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@
 
 import './matchers';
 
+export { deriveToHaveReceivedMessage, deriveToReceiveMessage } from './matchers';
 export { default } from './websocket';
 export { default as WS } from './websocket';
